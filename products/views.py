91636a40b09d3780c7e293522177d6c40f78f852
Cha_K23 <charlie@cartell.ie>
2021-01-26 09:15:29 +0000

    added products views and template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def all_lessons(request):
+    """ A view to show all lesson plans"""
+
+    products = Product.objects.all
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, "products/products.html", context)
